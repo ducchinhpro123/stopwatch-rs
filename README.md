30056ab8aa0cd2979e77764197e40e3cdfbe6cf0
C <chinh99562@donga.edu.vn>
2025-08-22 21:48:15 +0700

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+A stopwatch, input the number of minutes, the program will wait until those minutes have passed, then it will play a song.
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::io::{self, Write};
+use std::thread;
+use std::thread::sleep;
+use std::time::{Duration};
+
+use chrono::{Local, Timelike};
+use rodio::Decoder;
+use std::fs::File;
+
+fn main() {
+    let stdin = io::stdin();
+    let input_minutes = &mut String::new();
+    let mut num_of_minutes: i64 = 0;
+
+    match stdin.read_line(input_minutes) {
+        Ok(_) => {
+            let num_of_minutes_parsed = input_minutes.trim().parse::<i64>();
+            match num_of_minutes_parsed {
+                Ok(_) => {
+                    num_of_minutes = num_of_minutes_parsed.unwrap();
+                }
+                Err(_) => println!("Not a valid value, expect a number of minutes"),
+            }
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+
+    let now = Local::now();
+    let finish_time = now + chrono::Duration::minutes(num_of_minutes);
+
+    println!(
+        "Started at {:02}:{:02}, will finish at {:02}:{:02}",
+        now.hour(),
+        now.minute(),
+        finish_time.hour(),
+        finish_time.minute()
+    );
+
+    let handle = thread::spawn(move || {
+        for i in (1..num_of_minutes + 1).rev() {
+            print!("\r\x1B[2KTIME LEFT: {} minute", i);
+            io::stdout().flush().unwrap();
+            sleep(Duration::from_secs(60));
+        }
+    });
+
+    handle.join().unwrap();
+
+    // When it reaches here, start the notification by playing a song
+    let stream_handle =
+        rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
+    let sink = rodio::Sink::connect_new(&stream_handle.mixer());
+    let file = File::open("Kevin_MacLeod_-_Canon_in_D_Major(chosic.com).mp3").unwrap();
+    let source = Decoder::try_from(file).unwrap();
+    sink.append(source);
+    // stream_handle.mixer().add(source);
+    sink.sleep_until_end();
+    // std::thread::sleep(std::time::Duration::from_secs(5));
+}
